737d534ef64fd4abbba7395b363a5a72c9b91e2f
Yago Vinicius de Sales Alves <prof.yago.red@gmail.com>
2025-08-18 20:52:55 -0300

    Add essay correction endpoint (#48)

--- backend/routes/redacoes.js
+++ backend/routes/redacoes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const auth = require('../middleware/auth');
-const { upload, enviarRedacao, listarRedacoes } = require('../controllers/redacoesController');
+const { upload, enviarRedacao, listarRedacoes, corrigirRedacao } = require('../controllers/redacoesController');
 
 const router = express.Router();
 
 router.use(auth);
 router.post('/enviar', upload.single('file'), enviarRedacao);
 router.get('/professor', listarRedacoes);
+router.post('/:id/corrigir', corrigirRedacao);
 
 module.exports = router;
