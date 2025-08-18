69df7f49a94e9632e4fe512191caf826217f83fa
Yago Vinicius de Sales Alves <prof.yago.red@gmail.com>
2025-08-18 20:06:36 -0300

    feat: list redacoes for professor

--- backend/routes/redacoes.js
+++ backend/routes/redacoes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const auth = require('../middleware/auth');
-const { upload, enviarRedacao } = require('../controllers/redacoesController');
+const { upload, enviarRedacao, listarRedacoes } = require('../controllers/redacoesController');
 
 const router = express.Router();
 
 router.use(auth);
 router.post('/enviar', upload.single('file'), enviarRedacao);
+router.get('/professor', listarRedacoes);
 
 module.exports = router;
