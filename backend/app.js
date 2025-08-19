fef865678a522d1d71a0684f96ea08ff34e6cdad
Yago Vinicius de Sales Alves <prof.yago.red@gmail.com>
2025-08-18 23:33:22 -0300

    feat: add error handling middleware

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,7 @@ const gabaritoRoutes = require('./routes/gabaritos');
 const omrRoutes = require('./routes/omr');
 const redacoesRoutes = require('./routes/redacoes');
 const notificationRoutes = require('./routes/notifications');
+const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 app.use(cors());
@@ -44,5 +45,7 @@ if (isProd) {
     res.send('API running');
   });
 }
+
+app.use(errorHandler);
 
 module.exports = { app };
