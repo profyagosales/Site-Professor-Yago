10e8236ffde21de325f5d8f1f00c3671caab556f
Yago Vinicius de Sales Alves <prof.yago.red@gmail.com>
2025-08-22 22:29:04 -0300

    chore: secure deprecated students route (#146)

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.use((req, res) => {
-  res.status(410).json({
-    success: false,
-    message: 'Endpoint /students descontinuado. Utilize /classes/:id/students',
-    data: null,
-  });
-});
+router.use(
+  auth(),
+  (req, res) => {
+    res.status(410).json({
+      success: false,
+      message: 'Endpoint /students descontinuado. Utilize /classes/:id/students',
+      data: null,
+    });
+  }
+);
 
 module.exports = router;
