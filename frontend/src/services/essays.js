9301c91638c9510782ed049fc966c433a3e95575
Yago Vinicius de Sales Alves <prof.yago.red@gmail.com>
2025-08-23 12:49:02 -0300

    feat(frontend): add essays service

---
+++ frontend/src/services/essays.js
@@ -0,0 +1,27 @@
+import api from '@api';
+
+export const listThemes = (params) => api.get('/essays/themes', { params });
+export const createTheme = (data) => api.post('/essays/themes', data);
+export const updateTheme = (id, data) => api.patch(`/essays/themes/${id}`, data);
+
+export const uploadEssay = (formData) =>
+  api.post('/essays', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
+export const listEssays = (params) => api.get('/essays', { params });
+export const gradeEssay = (id, formData) =>
+  api.patch(`/essays/${id}/grade`, formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
+export const saveAnnotations = (id, data) =>
+  api.patch(`/essays/${id}/annotations`, data);
+
+export default {
+  listThemes,
+  createTheme,
+  updateTheme,
+  uploadEssay,
+  listEssays,
+  gradeEssay,
+  saveAnnotations,
+};
