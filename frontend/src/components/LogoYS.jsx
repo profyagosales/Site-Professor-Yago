576646026211ad9ef5821bbc4875fd2e897a66b0
Yago Vinicius de Sales Alves <prof.yago.red@gmail.com>
2025-08-23 01:06:38 -0300

    Add animated logo and login styles (#157)

---
+++ frontend/src/components/LogoYS.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './login.css';
+
+const words = ['Yago', 'School', 'Online'];
+
+const LogoYS = () => (
+  <div className="logo-ys">
+    <svg
+      width="120"
+      height="60"
+      viewBox="0 0 120 60"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <linearGradient id="logoGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+          <stop offset="0%" stopColor="#ff7e5f">
+            <animate
+              attributeName="stop-color"
+              values="#ff7e5f;#feb47b;#ff7e5f"
+              dur="6s"
+              repeatCount="indefinite"
+            />
+          </stop>
+          <stop offset="100%" stopColor="#feb47b">
+            <animate
+              attributeName="stop-color"
+              values="#feb47b;#ff7e5f;#feb47b"
+              dur="6s"
+              repeatCount="indefinite"
+            />
+          </stop>
+        </linearGradient>
+      </defs>
+      <text
+        x="10"
+        y="45"
+        fontSize="45"
+        fontWeight="700"
+        fill="url(#logoGradient)"
+      >
+        YS
+      </text>
+    </svg>
+    <div className="rotating-words">
+      {words.map((word, index) => (
+        <span key={word} style={{ '--i': index }}>{word}</span>
+      ))}
+    </div>
+  </div>
+);
+
+export default LogoYS;
